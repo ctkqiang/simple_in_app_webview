5865e02018705fed5ea3a97b5673fce2d81a24e6
John Melody Me <Johnmelodyme@yandex.com>
2022-05-22 14:53:37 +0800

    Initial commit

---
+++ example/android/app/src/main/kotlin/uk/co/johnmelodyme/simpleinappwebview/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package uk.co.johnmelodyme.simpleinappwebview.example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/simple_in_app_webview.dart
@@ -0,0 +1,7 @@
+library simple_in_app_webview;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
