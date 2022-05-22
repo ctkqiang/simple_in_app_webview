c3f0fa206e91f65202bd510572304eae78a8a7b4
John Melody Me <Johnmelodyme@yandex.com>
2022-05-22 15:28:21 +0800

    Readjusted project structure

--- lib/simple_in_app_webview.dart
+++ lib/simple_in_app_webview.dart
@@ -1,7 +1,34 @@
 library simple_in_app_webview;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+///
+///
+///
+import 'package:flutter/material.dart';
+
+/// @author : John Melody Me <johnmelodyme@yahoo.com>
+/// ignore: must_be_immutable
+class SimpleWebView extends StatefulWidget {
+  String? url;
+  Color? appBarColour;
+
+  /// [SimpleWebView]
+  ///
+  SimpleWebView({
+    Key? key,
+    @required this.url,
+    this.appBarColour = Colors.white,
+  }) : super(key: key);
+
+  @override
+  State<SimpleWebView> createState() => _SimpleWebViewState();
+}
+
+class _SimpleWebViewState extends State<SimpleWebView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SafeArea(child: Text('')),
+    );
+  }
 }
